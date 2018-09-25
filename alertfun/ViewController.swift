b4e09a8a4cc92d30b1ab31c208ecd10722caf5fb
Chucka, Zachary Tyler <zchucka@hk223-27739.local>
2018-09-25 09:58:21 -0700

    Initial Commit

---
+++ alertfun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alertfun
+//
+//  Created by Chucka, Zachary Tyler on 9/25/18.
+//  Copyright © 2018 Chucka, Zachary Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
